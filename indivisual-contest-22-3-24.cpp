c74739759a06e1d8ab91816c8579e2d87f3d5854
Khalid Hasan <83982215+khalidhasananik@users.noreply.github.com>
2024-04-05 16:21:18 +0600

    indi 22-3-24

--- indivisual-contest-22-3-24.cpp
+++ indivisual-contest-22-3-24.cpp
@@ -14,15 +14,145 @@ using namespace std;
 
 const double PI = 3.1415926535897;
 
-void E();
+void H();
 
 signed main()
 {
     fast_io;
 
-    std_io;
+    // std_io;
 
-    E();
+    H();
+}
+
+int numberOfDivisors(int n)
+{
+    int cnt = 0;
+
+    for (int i = 1; i * i <= n; i++)
+
+        if (i * i == n)
+
+            cnt++;
+
+        else if (n % i == 0)
+
+            cnt += 2;
+
+    return cnt;
+}
+
+vector<int> v;
+
+void sort()
+{
+    map<int, int, greater<int>> mp; // decreasing order
+
+    for (int i = 1; i <= 1000; i++)
+
+        mp[i] = numberOfDivisors(i);
+
+    multimap<int, int> MM;
+
+    for (auto &it : mp)
+
+        MM.insert({it.second, it.first});
+
+    for (auto &it : MM)
+
+        v.push_back(it.second);
+}
+
+void H()
+{
+    sort();
+
+    int tc;
+
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        int n;
+
+        cin >> n;
+
+        cout << "Case " << i << ": " << v[n - 1] << endl;
+    }
+}
+
+void G()
+{
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+
+    for (int &i : arr)
+
+        cin >> i;
+
+    sort(arr, arr + n);
+
+    stack<int> s;
+
+    for (int i = 0; i < n; i++)
+
+        s.push(arr[i]);
+
+    int ans = 0;
+
+    int tc = n / 3;
+
+    while (tc--)
+    {
+        s.pop();
+
+        ans += s.top();
+
+        s.pop();
+    }
+
+    cout << ans << endl;
+}
+
+void F()
+{
+    int tc;
+
+    cin >> tc;
+
+    while (tc--)
+    {
+        int n;
+
+        cin >> n;
+
+        int arr[n];
+
+        for (int &i : arr)
+
+            cin >> i;
+
+        int ans = 0;
+
+        for (int k = 1; k <= n; k++)
+        {
+            if (n % k == 0)
+            {
+                int g = 0;
+
+                for (int i = 0; i + k < n; i++)
+
+                    g = __gcd(g, abs(arr[i] - arr[i + k]));
+
+                ans += (g != 1);
+            }
+        }
+
+        cout << ans << endl;
+    }
 }
 
 void E()
@@ -30,8 +160,6 @@ void E()
     int n;
 
     cin >> n;
-
-    list<int> tim;
 
     multimap<int, string> mp;
 
@@ -43,29 +171,10 @@ void E()
 
         int time = h * (60 * 60) + m * 60 + s;
 
-        string sh = to_string(h);
+        string str = to_string(h) + " " + to_string(m) + " " + to_string(s);
 
-        string sm = to_string(m);
-
-        string ss = to_string(s);
-
-        string str = sh;
-
-        str += ' ' + sm + ' ' + ss;
-
-        tim.push_back(time);
-
-        mp[time] = str;
+        mp.insert({time, str});
     }
-
-    // tim.sort();
-
-    // while (!tim.empty())
-    // {
-    //     cout << mp[tim.front()] << endl;
-
-    //     tim.pop_front();
-    // }
 
     for (auto it : mp)
 
